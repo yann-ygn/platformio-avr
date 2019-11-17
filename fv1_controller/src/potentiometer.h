3ae448db184e271f3b30e80156a8e572cda89f59
effectspcbs <yann.gaudineau@gmail.com>
2019-11-17 22:33:32 +0100

    20191117/10:33

--- fv1_controller/src/potentiometer.h
+++ fv1_controller/src/potentiometer.h
@@ -4,37 +4,45 @@
 #define POTENTIOMETER_H
 
 /**
- * @brief 
- * 
+ * @brief An analog potentiometer connected to an ADC pin on the µC. The object is instantiated with the pin number used.
+ * The potentiometer needs to be setup, can check if its value changed since the last cycle and return its current value. 
  */
 class Pot
 {
     private:
-        /**
-         * @brief blah
-         * 
-         */
         byte m_pin;
         int m_currPotValue;
         int m_lastPotValue;
 
     public:
-    /**
-     * @brief Construct a new Pot object
-     * 
-     * @param pin 
-     */
-     
+        /**
+         * @brief Construct a new Pot object
+         * 
+         * @param pin The physical pin #
+         */     
         Pot(byte pin) : m_pin(pin)
         {
 
         }
+
         /**
-         * @brief test
-         * 
+         * @brief Setup the µC's pin and read the current value
          */
         void potSetup();
+
+        /**
+         * @brief Get the pot current value
+         * 
+         * @return int 
+         */
         int getPotValue();
+
+        /**
+         * @brief Check if the pot value has changed since the last cycle
+         * 
+         * @return true 
+         * @return false 
+         */
         bool potTurned();
 };
 
