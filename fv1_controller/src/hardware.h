0f92f74e0fc02c887e313183f35c0260a238fa6f
effectspcbs <yann.gaudineau@gmail.com>
2020-04-02 21:15:37 +0200

    20200402/09:15

---
+++ fv1_controller/src/hardware.h
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+class Hardware
+{
+    private:
+
+    public:
+        /**
+         * @brief 
+         */
+        void hardwareSetup();
+};
+
+#endif
