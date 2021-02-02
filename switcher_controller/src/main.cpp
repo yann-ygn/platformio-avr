35a9142262e61a7cc54f583d4033d934cbb631c5
unknown <yann.gaudineau@gmail.com>
2021-02-02 22:17:41 +0100

    20210202/10:17

--- switcher_controller/src/main.cpp
+++ switcher_controller/src/main.cpp
@@ -1,24 +1,31 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-uint8_t csPin = 49;
-uint16_t data[16] = {0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF, 0XFFFF};
+uint8_t csPin = 1;
+uint16_t data[16] = {0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000};
 
 void setup() 
 {
 	pinMode(csPin, OUTPUT);
 	digitalWrite(csPin, HIGH);
 	delay(1000);
-	
-	SPI.begin();
-}
 
-void loop()
-{
-	SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
+	SPI.begin();
+
+	SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
 
 	for (int16_t y = 15; y >= 0; y--) {
         SPI.transfer16(data[y]);
     }
     SPI.endTransaction();
+	
+    delayMicroseconds(8);
+    digitalWrite(csPin, LOW);
+    delayMicroseconds(8);
+    digitalWrite(csPin, HIGH);
+}
+
+void loop()
+{
+	
 }
