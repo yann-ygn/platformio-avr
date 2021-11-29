91da4b4d1e0b443f42ebb5920956e0d0e27f6aeb
effectspcbs <yann.gaudineau@gmail.com>
2021-11-29 20:54:39 +0000

    20211129/20:54

--- switcher_controller/src/hardware.cpp
+++ switcher_controller/src/hardware.cpp
@@ -1,1 +1,56 @@
 #include "hardware.h"
+#include "menu.h"
+#include "encoder.h"
+#include "led.h"
+#include "switch.h"
+#include "loops.h"
+
+Encoder selector(32, 33);
+TemporarySwitch selectorSwitch(34, 1000);
+TemporarySwitch editSwitch(30, 1000);
+Led editSwitchLed(31);
+Loops loops(6);
+
+void Hardware::hardwareSetup()
+{
+  Serial.begin(115200);
+  selector.encoderSetup();
+  selectorSwitch.tempSwitchSetup();
+  editSwitch.tempSwitchSetup();
+  editSwitchLed.ledSetup();
+}
+
+void Hardware::hardwarePoll()
+{
+
+}
+
+void Hardware::resetHardwareTriggers()
+{
+
+}
+
+bool Hardware::getSelectorMove()
+{
+    return m_selectorMove;
+}
+
+bool Hardware::getSelectorwitchPress()
+{
+    return m_selectorSwitchPress;
+}
+
+bool Hardware::getSelectorSwitchLongPress()
+{
+    return m_selectorSwitchLongPress;
+}
+
+bool Hardware::getEditSwitchPress()
+{
+    return m_editSwitchPress;
+}
+
+bool Hardware::getEditSwitchLongPress()
+{
+    return m_editSwitchLongPress;
+}
--- switcher_controller/src/hardware.h
+++ switcher_controller/src/hardware.h
@@ -4,5 +4,26 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
+class Hardware
+{
+    private:
+        // Hardware Triggers
+        bool m_selectorMove = false;
+        bool m_selectorSwitchPress = false;
+        bool m_selectorSwitchLongPress = false;
+        bool m_editSwitchPress = false;
+        bool m_editSwitchLongPress = false;
+
+    public:
+        void hardwareSetup();
+        void hardwarePoll();
+        void resetHardwareTriggers();
+
+        bool getSelectorMove();
+        bool getSelectorwitchPress();
+        bool getSelectorSwitchLongPress();
+        bool getEditSwitchPress();
+        bool getEditSwitchLongPress();
+};
 
 #endif
